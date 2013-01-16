dfd002de3247595c9ce9777fe0aadf96cfe815d8
godspeed1989 <tongxinjichu@gmail.com>
2013-01-16 23:14:28 +0800

    update pc speaker

--- pc_speaker/speaker.c
+++ pc_speaker/speaker.c
@@ -1,27 +1,47 @@
 #include <linux/module.h>
 #include <asm/io.h>
 
-u8 tmp;
+#define FREQ 1193180
+static u8 port61;
+
+
+static void setup_freq(int div)
+{
+	outb(0xb6, 0x43);
+	outb((u8) (FREQ / div), 0x42);
+	outb(((u8) (FREQ / div) >> 8), 0x42);
+}
+
+static void play_sound(void)
+{
+	setup_freq(1000);
+ 	//enable the PC speaker
+	port61 = inb(0x61);
+	if (port61 != (port61 | 3))
+	{
+		outb(port61 | 3, 0x61);
+	}
+	else
+	{
+		printk("%s: PC speaker already open.\n", __FUNCTION__);
+	}
+}
 
 int __init pcsp_init(void)
 {
 	printk("%s\n", __FUNCTION__);
-
- 	outb(0x43, 0xb6);
- 	outb(0x42, (u8) (1193180 / 1000) );
- 	outb(0x42, (u8) ((1193180 / 1000) >> 8));
- 	
-	tmp = inb(0x61);
-	if (tmp != (tmp | 3))
-	{
-		outb(0x61, tmp | 3);
-	}
+	play_sound();
 	return 0;
 }
 module_init(pcsp_init);
 
 void __exit pcsp_exit(void)
 {
-	outb(0x61, tmp);
+	printk("%s: restore the state of speaker.\n", __FUNCTION__);
+	outb(port61, 0x61);
 }
 module_exit(pcsp_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("godspeed1989");
+
