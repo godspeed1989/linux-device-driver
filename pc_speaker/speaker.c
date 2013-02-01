bb9a9aef7f7137c2ed31f109c67b1e82c8c154d9
godspeed1989 <tongxinjichu@gmail.com>
2013-02-01 13:47:05 +0800

    update pc speaker

--- pc_speaker/speaker.c
+++ pc_speaker/speaker.c
@@ -2,29 +2,40 @@
 #include <asm/io.h>
 
 #define FREQ 1193180
-static u8 port61;
 
-
-static void setup_freq(int div)
+static void setup_freq(int nfreq)
 {
+	u32 div;
+	div = FREQ / nfreq;
 	outb(0xb6, 0x43);
-	outb((u8) (FREQ / div), 0x42);
-	outb(((u8) (FREQ / div) >> 8), 0x42);
+	outb((u8) (div), 0x42);
+	outb((u8) (div >> 8), 0x42);
 }
 
-static void play_sound(void)
+static u8 port61;
+static void enable(void)
 {
-	setup_freq(1000);
- 	//enable the PC speaker
 	port61 = inb(0x61);
-	if (port61 != (port61 | 3))
+	printk("%s: port61 = %x\n", __FUNCTION__, port61);
+	if (0x3 != (port61 & 3))
 	{
+		printk("%s: set port61 to %x\n", __FUNCTION__, port61|3);
 		outb(port61 | 3, 0x61);
 	}
 	else
 	{
 		printk("%s: PC speaker already open.\n", __FUNCTION__);
 	}
+}
+static void restore(void)
+{
+	outb(port61, 0x61);
+}
+
+static void play_sound(void)
+{
+	//setup_freq(800);
+ 	enable();
 }
 
 int __init pcsp_init(void)
@@ -38,7 +49,7 @@ module_init(pcsp_init);
 void __exit pcsp_exit(void)
 {
 	printk("%s: restore the state of speaker.\n", __FUNCTION__);
-	outb(port61, 0x61);
+	restore();
 }
 module_exit(pcsp_exit);
 
